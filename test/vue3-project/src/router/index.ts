730f5b29bfaae5dcd62b2b70bfb01ed777f8d2bd
sunyan <sunyan@joygames.cc>
2024-05-29 14:56:49 +0800

    feat: 路由搭建

--- test/vue3-project/src/router/index.ts
+++ test/vue3-project/src/router/index.ts
@@ -1,23 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {
+  createRouter,
+  createWebHashHistory
+  // createWebHistory
+} from 'vue-router'
+import { dynamicRouterMap } from './router.dynamic'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+  history: createWebHashHistory(),
+  routes: dynamicRouterMap,
+  scrollBehavior() {
+    return {
+      top: 0,
+      behavior: 'smooth'
     }
-  ]
+  }
 })
 
-export default router
+export { router as default, dynamicRouterMap }
